f0cc9a1688c024df6edf951b6c951c02bf4e843f
olivermehr <68856567+olivermehr@users.noreply.github.com>
2025-06-09 16:20:37 +0100

    WIP on Jooce price

---
+++ src/JOOCE/JoocePrice.ts
@@ -0,0 +1,29 @@
+import { Address, BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { UniV3Pool } from "../../generated/joocePool/UniV3Pool"
+import { createOrLoadJoocePriceEntity } from "../EntityCreation";
+
+
+
+
+export function handleJoocePrice(block: ethereum.Block): void {
+    const jooceEthPoolAddress = Address.fromString("0xa491C1F081f3c44047b5711f883d20342EdeF5e1")
+    const ethUsdcPoolAddress = Address.fromString("0xd0b53D9277642d899DF5C87A3966A349A798F224")
+    const jooceEthPoolContract = UniV3Pool.bind(jooceEthPoolAddress)
+    const ethUsdcPoolContract = UniV3Pool.bind(ethUsdcPoolAddress)
+    const jooceEthPrice = getPrice(jooceEthPoolContract, 18, 18)
+    const ethUsdcPrice = getPrice(ethUsdcPoolContract, 18, 6)
+    const jooceUsdcPrice = jooceEthPrice.times(ethUsdcPrice)
+
+    createOrLoadJoocePriceEntity(block.timestamp, jooceUsdcPrice)
+}
+
+
+function getPrice(pool: UniV3Pool, decimalsToken0: u8, decimalsToken1: u8): BigDecimal {
+    const sqrtPrice = pool.slot0().getSqrtPriceX96()
+    const priceRaw = sqrtPrice.div(BigInt.fromI32(2).pow(96)).pow(2)
+    const scaledPrice = priceRaw.times(BigInt.fromI32(10).pow(decimalsToken0 - decimalsToken1))
+
+    return scaledPrice.toBigDecimal()
+
+
+}
--- src/JOOCE/JooceReward.ts
+++ src/JOOCE/JooceReward.ts
@@ -1,16 +1,10 @@
 import { log, Address } from "@graphprotocol/graph-ts"
 import { Transfer as TransferEvent } from "../../generated/ERC20/ERC20"
-import { JooceFeeReward as TransferEntity } from "../../generated/schema"
+import { createOrLoadJooceRewardEntity } from "../EntityCreation"
 
 export function handleJooceTransfer(event: TransferEvent): void {
     log.debug("HandleJooceTransfer event hash: {} from: {} logIndex: {} to: {}", [event.transaction.hash.toHexString(), event.params.from.toHexString(), event.logIndex.toHexString(), event.params.to.toHexString()])
-    const id = `${event.transaction.hash.toHex()}-${event.logIndex.toString()}`
     if (event.params.to == Address.fromString("0x004fd4bcc4c9989c18a1a1463241b7ccec9f7051")) {
-        const reward = new TransferEntity(id)
-        reward.from = event.params.from
-        reward.to = event.params.to
-        reward.value = event.params.value
-        reward.timestamp = event.block.timestamp
-        reward.save()
+    createOrLoadJooceRewardEntity(event.transaction.hash,event.logIndex,event.block.timestamp,event.params.from, event.params.to,event.params.value)
     }
 }
