b8a1a92ece6872c8beb418bd96c5debc2ed2d8ac
Oliver Mehr <68856567+olivermehr@users.noreply.github.com>
2025-06-10 01:56:00 +0100

    Use equals for JooceReward recipient check

--- src/JOOCE/JooceReward.ts
+++ src/JOOCE/JooceReward.ts
@@ -4,7 +4,7 @@ import { createOrLoadJooceRewardEntity } from "../EntityCreation"
 
 export function handleJooceTransfer(event: TransferEvent): void {
     log.debug("HandleJooceTransfer event hash: {} from: {} logIndex: {} to: {}", [event.transaction.hash.toHexString(), event.params.from.toHexString(), event.logIndex.toHexString(), event.params.to.toHexString()])
-    if (event.params.to == Address.fromString("0x004fd4bcc4c9989c18a1a1463241b7ccec9f7051")) {
+    if (event.params.to.equals(Address.fromString("0x004fd4bcc4c9989c18a1a1463241b7ccec9f7051"))) {
     createOrLoadJooceRewardEntity(event.transaction.hash,event.logIndex,event.block.timestamp,event.params.from, event.params.to,event.params.value)
     }
 }
